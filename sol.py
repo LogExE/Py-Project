bfcf7916f966bb11427a8a45e9359d03bcad5a9e
Vladimir <vladimir220022@mail.ru>
2019-02-09 22:37:38 +0400

    v.2 localized

--- sol.py
+++ sol.py
@@ -1,15 +1,15 @@
 n, m = int(input("N: ")), int(input("M: "))
-print("Матрица:")
+print("Matrix:")
 mat = tuple(list(map(int, input().split(' ', n))) for i in range(m))
 minAs = tuple(min(enumerate(arr), key = lambda x: x[1]) for arr in mat)
 maxmin = max(minAs, key = lambda x: x[1])
-# zip(*mat) - транспонированная исходная матрица
-# lambda x: x[1] - возращает второй элемент пары
+# zip(*mat) - transposed matrix
+# lambda x: x[1] - returns the second element of pair
 maxBs = tuple(max(enumerate(arr), key = lambda x: x[1]) for arr in zip(*mat))
 minmax = min(maxBs, key = lambda x: x[1])
 print(f"maxmin: {maxmin[1]}, minmax: {minmax[1]}")
 if minmax[1] == maxmin[1]:
-    print(f"Есть седловая точка, цена игры: {minmax[1]}")
-    print(f"Номер чистых cтратегий I: {minmax[0] + 1}, II: {maxmin[0] + 1}")
+    print(f"Saddle point exists, cost of game is {minmax[1]}")
+    print(f"Numbers of pure strategys I: {minmax[0] + 1}, II: {maxmin[0] + 1}")
 else:
-    print("Седловой точки нет.")
+    print("No saddle point.")
