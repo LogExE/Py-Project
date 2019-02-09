94c9e407e9de5e2224cd4bc88650a0ea6772df2d
Vladimir <vladimir220022@mail.ru>
2019-02-09 22:31:51 +0400

    v. 2

--- sol.py
+++ sol.py
@@ -1,12 +1,15 @@
-n, m = int(input("N:")), int(input("M:"))
-mat = [list(map(int, input().split(' ', m))) for i in range(n)]
-minAs = set()
-for arr in mat:
-    minAs.add(min(arr))
-maxmin = max(minAs)
-maxBs = set()
-for arr in zip(*mat):
-    maxBs.add(max(arr))
-minmax = min(maxBs)
-print(f"maxmin: {maxmin}, minmax: {minmax}")
-print("Опорная точка есть" if minmax == maxmin else "Опорной точки нет")
+n, m = int(input("N: ")), int(input("M: "))
+print("Матрица:")
+mat = tuple(list(map(int, input().split(' ', n))) for i in range(m))
+minAs = tuple(min(enumerate(arr), key = lambda x: x[1]) for arr in mat)
+maxmin = max(minAs, key = lambda x: x[1])
+# zip(*mat) - транспонированная исходная матрица
+# lambda x: x[1] - возращает второй элемент пары
+maxBs = tuple(max(enumerate(arr), key = lambda x: x[1]) for arr in zip(*mat))
+minmax = min(maxBs, key = lambda x: x[1])
+print(f"maxmin: {maxmin[1]}, minmax: {minmax[1]}")
+if minmax[1] == maxmin[1]:
+    print(f"Есть седловая точка, цена игры: {minmax[1]}")
+    print(f"Номер чистых cтратегий I: {minmax[0] + 1}, II: {maxmin[0] + 1}")
+else:
+    print("Седловой точки нет.")
